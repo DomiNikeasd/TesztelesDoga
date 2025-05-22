d0c402e9fd8d2a26bf9f279fc454f3176fd923db
DomiNikeasd <85038166+DomiNikeasd@users.noreply.github.com>
2025-05-22 13:49:39 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class EvszakKiszamolo
+    {
+        public static string Evszak(int honap)
+        {
+            while (true)
+            {
+                Console.WriteLine("Adj meg egy szamot 1 és 12 kozott");
+                int asd = int.Parse(Console.ReadLine());
+
+                if (asd > 12 || asd < 1)
+                {
+                    Console.WriteLine("nem jouu");
+                }
+                else if (asd == 12 || asd == 1 || asd == 2)
+                {
+                    Console.WriteLine("A honapod tel");
+                }
+                else if (asd == 3 || asd == 4 || asd == 5)
+                {
+                    Console.WriteLine("A honapod tavasz");
+                }
+                else if (asd == 6 || asd == 7 || asd == 8)
+                {
+                    Console.WriteLine("A honapod nyár");
+                }
+                else if (asd == 9 || asd == 10 || asd == 11)
+                {
+                    Console.WriteLine("A honapod ősz");
+                }
+                else
+                {
+                    Console.WriteLine("nem jooooooooouu");
+                }
+                Console.ReadKey();
+            }
+        }
+       
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var asd = new Program();
+            string result = "Tél";
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var asd = new Program();
+            string result = "Tavasz";
+            Assert.AreEqual(result, 2);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var asd = new Program();
+            string result = "Ősz";
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var asd = new Program();
+            string result = "Nyár";
+            Assert.AreEqual(result, 4);
+        }
+    }
+}
